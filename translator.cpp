fcf2e9886c96895b211b3a3024dbeefef745ceb7
Artyom Borisov <2001raf@gmail.com>
2022-12-06 18:09:29 +0500

    Added initial state, terminal state, and transitions count reading.

--- translator.cpp
+++ translator.cpp
@@ -3,36 +3,64 @@
 #include <string>
 #include <iomanip>
 #include <bitset>
+#include <map>
+
+void write_in_little_endian(std::fstream& fs, uint32_t number){
+                fs << (unsigned char)(number & 255) <<
+                       (unsigned char)((number >> 8 ) & 255) <<
+                       (unsigned char)((number >> 16) & 255) <<
+                       (unsigned char)((number >> 24) & 255);
+}
 
 int main(int argc, char* argv[]){
 	if (argc < 3){
 		std::cout << "Not enough arguments\n";
 		return 1;
 	}
+
 	std::fstream input_file;
 	std::fstream output_file;
+
 	input_file.open(argv[1], std::ios::in);
+
 	if (!input_file){
 		std::cout << "Can not open " << argv[1] << '\n';
 		return 1;
 	}
+
 	output_file.open(argv[2], std::ios::out);
+
 	if (!output_file){
 		std::cout << "Can not open" << argv[2] << '\n';
 		input_file.close();
 		return 1;
 	}
+
 	uint32_t state_count;
-	int state_max_length = 10;
+
 	input_file >> state_count;
-	output_file << (state_count & 255) <<
-                       ((state_count >> 8 ) & 255) <<
-                       ((state_count >> 16) & 255) <<
-                       ((state_count >> 24) & 255);
-	for (int i = 0; i != state_count; ++i){
+	write_in_little_endian(output_file, state_count);
+
+	int state_max_length = 10;
+	std::map<std::string, uint32_t> states_order;
+
+	for (uint32_t i = 0; i != state_count; ++i){
 		std::string state;
 		input_file >> state;
 		output_file << std::setw(10) << std::setfill('\0') << state;
+		states_order[state] = i;
 	}
+
+	std::string initial_state, terminal_state;
+	input_file >> initial_state >> terminal_state;
+
+	write_in_little_endian(output_file, states_order[initial_state]);
+	write_in_little_endian(output_file, states_order[terminal_state]);
+
+	uint32_t transitions_count;
+
+	input_file >> transitions_count;
+	write_in_little_endian(output_file, transitions_count);
+
 	return 0;
 }
