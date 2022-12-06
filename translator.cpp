a5cdbeb9442166e028cc9adbef74ac436eecc4af
Artyom Borisov <2001raf@gmail.com>
2022-12-06 18:40:50 +0500

    Added sorted transitions writing in the translator.

--- translator.cpp
+++ translator.cpp
@@ -39,7 +39,7 @@ int main(int argc, char* argv[]){
 	uint32_t state_count;
 
 	input_file >> state_count;
-	write_in_little_endian(output_file, state_count);
+	//write_in_little_endian(output_file, state_count);
 
 	int state_max_length = 10;
 	std::map<std::string, uint32_t> states_order;
@@ -47,20 +47,36 @@ int main(int argc, char* argv[]){
 	for (uint32_t i = 0; i != state_count; ++i){
 		std::string state;
 		input_file >> state;
-		output_file << std::setw(10) << std::setfill('\0') << state;
+		//output_file << std::setw(10) << std::setfill('\0') << state;
 		states_order[state] = i;
 	}
 
 	std::string initial_state, terminal_state;
 	input_file >> initial_state >> terminal_state;
 
-	write_in_little_endian(output_file, states_order[initial_state]);
-	write_in_little_endian(output_file, states_order[terminal_state]);
+	//write_in_little_endian(output_file, states_order[initial_state]);
+	//write_in_little_endian(output_file, states_order[terminal_state]);
 
 	uint32_t transitions_count;
 
 	input_file >> transitions_count;
-	write_in_little_endian(output_file, transitions_count);
+	//write_in_little_endian(output_file, transitions_count);
+
+	std::map<std::pair<uint32_t, unsigned char>, std::tuple<uint32_t, unsigned char, unsigned char>> transitions;
+
+	for (int i = 0; i != transitions_count; ++i){
+		std::string from_state, to_state;
+		unsigned char from_ch, to_ch, move;
+		input_file >> from_state >> from_ch >> to_state >> to_state >> to_ch >> move;
+		transitions[std::make_pair(states_order[from_state], from_ch)] = std::make_tuple(states_order[to_state], to_ch, move);
+	}
+
+	for (auto transition: transitions){
+		write_in_little_endian(output_file, transition.first.first);
+		output_file << transition.first.second;
+		write_in_little_endian(output_file, std::get<0>(transition.second));
+		output_file << std::get<1>(transition.second) << std::get<2>(transition.second);
+	}
 
 	return 0;
 }
